af65230d6bc302cdf36db5c5428e7d60586db1d6
deleriouseel <kristin@northcountrychapel.com>
2024-10-04 14:25:36 -0700

    initial commit

---
+++ encodeVideo/delete_oldFiles.py
@@ -0,0 +1,56 @@
+import os
+import shutil
+import time
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    filename="filename.log",
+)
+
+
+# Set the directories
+source_dir = r"D:\Studies"
+network_dir = r"\\DocuSynology\video"
+
+# Get the current time
+now = time.time()
+
+# Calculate the cutoff time for files older than 3 weeks
+cutoff_time = now - (3 * 7 * 24 * 60 * 60)
+
+# Loop through all folders in the source directory
+for foldername in os.listdir(source_dir):
+    folder_path = os.path.join(source_dir, foldername)
+
+    # Check if it's a directory
+    if os.path.isdir(folder_path):
+        # Loop through all files in the folder
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+
+            # Check if it's a file and .mp4
+            if os.path.isfile(file_path) and filename.lower().endswith('.mp4'):
+                # Get the last modified time of the file
+                file_modified_time = os.path.getmtime(file_path)
+                
+
+                # Check if the file is older than 3 weeks
+                if file_modified_time < cutoff_time:
+                    # Create the network target path
+                    network_target_file_path = os.path.join(network_dir, foldername, filename)
+                    logging.debug(f"Old files found: {filename}")
+
+                    # Check if the file exists on the network
+                    if os.path.isfile(network_target_file_path):
+                        logging.info(f"Deleting: {file_path}")
+                        # If it exists, delete the local copy
+                        os.remove(file_path)
+                        print(f"Deleted local copy: {file_path}")
+                    else:
+                        # If it doesn't exist, move it to the network folder
+                        os.makedirs(os.path.dirname(network_target_file_path), exist_ok=True)
+                        shutil.move(file_path, network_target_file_path)
+                        logging.info(f"Moved: {file_path} to {network_target_file_path}")
---
+++ encodeVideo/move_files.py
@@ -0,0 +1,57 @@
+import os
+import shutil
+import time
+from datetime import datetime, timedelta
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    filename="filename.log",
+)
+
+# Set the directory to scan
+source_dir = r"D:\Studies"
+network_dir = r"\\DocuSynology\video"
+
+
+# Get the current time
+now = time.time()
+
+# Fles older than 6 days
+cutoff_time = now - (6 * 24 * 60 * 60)
+
+# Loop through all files in the source directory
+for filename in os.listdir(source_dir):
+    file_path = os.path.join(source_dir, filename)
+    logging.debug(file_path)
+
+    
+    if os.path.isfile(file_path):
+        file_modified_time = os.path.getmtime(file_path)
+
+        if file_modified_time < cutoff_time:
+            # Extract the first 6 characters of the filename
+            target_folder = filename[:6]
+            logging.debug(target_folder)
+            target_path = os.path.join(source_dir, target_folder)
+            network_target_path = os.path.join(network_dir, target_folder)
+
+            # Create the target folder if it doesn't exist
+            os.makedirs(target_path, exist_ok=True)
+            os.makedirs(network_target_path, exist_ok=True)
+                        
+            # Copy the file to the backup folder
+            logging.info(f"Copying {filename} to {network_target_path}")
+            network_target_file_path = os.path.join(network_target_path, filename)
+            shutil.copy2(file_path, network_target_file_path)
+            logging.info(f"Finished copying {filename}")
+            #Move to local folder
+            logging.info(f"Moving {filename} to {target_path}")
+            shutil.move(file_path, os.path.join(target_path, filename))
+            logging.info(f"Finished moving {filename}")
+
+
+
+print("File moving process completed.")
