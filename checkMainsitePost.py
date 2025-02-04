fd3be732359273c1c293d785b4ec1f04c69bb9ab
deleriouseel <kristin@northcountrychapel.com>
2025-02-04 08:49:52 -0800

    fix fallback to previous study

--- checkMainsitePost.py
+++ checkMainsitePost.py
@@ -44,6 +44,7 @@ def getPost(url):
 
 def checkWPPost():
     logging.info("checkWPPost")
+    
     required_category = 2420  # Video Studies category ID
     bible_categories = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 
     33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 547, 548, 531, 627, 764, 821, 
@@ -51,23 +52,33 @@ def checkWPPost():
     2002, 2003, 2004, 2005, 2006, 2007, 2009, 2010, 2011, 2012, 2069, 2070, 2071, 2072, 
     2073, 2074, 2163, 2170, 2174, 2238, 2243, 2192, 2420, 2513, 2524]
 
+
     posts = getPost(os.getenv("WP_API_URL") + "posts?categories=48&per_page=1")
     if not posts:
         logging.error("Failed to retrieve posts from WordPress API")
         return
-
+    
     latest_post = posts[0]
+
+    from datetime import datetime, timezone
+    post_date = datetime.fromisoformat(latest_post['date'].replace('Z', '+00:00'))
+    current_date = datetime.now(timezone.utc)
+
+
+    if post_date.date() != current_date.date():
+        logging.error(f"No Audio Study post found for {current_date.date()}")
+        send_email(latest_post, None, None, f"No Audio Study post found for {current_date.date()}")
+        return None
+
     is_valid, content_length, last_modified, validation_message = validate_post(latest_post, required_category, bible_categories)
 
     if is_valid:
-        logging.info("Sending email with post details...")
+        logging.info("Latest post is valid.")
         send_email(latest_post, content_length, last_modified)
-        print("Latest post is valid.")
         return latest_post
     else:
-        logging.info("Sending email with validation failure...")
+        logging.info("No valid post found.")
         send_email(latest_post, content_length, last_modified, validation_message)
-        print("No valid post found.")
         return None
     
     
